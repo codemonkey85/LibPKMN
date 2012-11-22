04f0053943b97924e3593e4ba60e998a26a6e02c
Nicholas Corgan <n.corgan@gmail.com>
2012-11-22 07:28:04 -0800

    bin: added sample teams to be used with analyze_pkmn_team
    ** Red and Blue from Pokemon World Tournament (diverse teams)
    ** Necessary Pokemon added to National Pokedex

--- lib/base_pkmn.cpp
+++ lib/base_pkmn.cpp
@@ -152,12 +152,34 @@ base_pkmn get_pokemon(std::string pkmn_name)
     std::map<std::string, base_pkmn> nat_pokedex = boost::assign::map_list_of
         ("bulbasaur", base_pkmn("Bulbasaur",1,"Seed", "Grass","Poison", "Overgrow","Chlorophyll","",
                                 0.7,6.9, 0.875,0.125, 45,49,49,65,65,45, 0,0,0,1,0,0, 64)) //RBG,FRLG starter
+        ("venusaur", base_pkmn("Venusaur",3,"Seed", "Grass","Poison", "Overgrow","Chlorophyll","",
+                                2.0,100.0, 0.875,0.125, 80,82,83,100,100,80, 0,0,0,2,1,0, 236))
         ("charmander", base_pkmn("Charmander",4,"Lizard", "Fire","", "Blaze","Solar Power","",
                                  0.6,8.5, 0.875,0.125, 39,52,43,60,50,65, 0,0,0,0,0,1, 62)) //RBG,FRLG starter
+        ("charizard", base_pkmn("Charizard",6,"Flame", "Fire","Flying", "Blaze","Solar Power","",
+                                 1.7,90.5, 0.875,0.125, 78,84,78,109,89,100, 0,0,0,3,0,0, 240))
         ("squirtle", base_pkmn("Squirtle",7,"Tiny Turtle", "Water","", "Torrent","Rain Dish","",
                                0.5,9.0, 0.875,0.125, 44,48,65,50,64,43, 0,0,1,0,0,0, 63)) //RBG,FRLG starter
+        ("blastoise", base_pkmn("Blastoise",9,"Shellfish", "Water","", "Torrent","Rain Dish","",
+                               1.6,85.5, 0.875,0.125, 79,83,100,85,105,78, 0,0,0,0,3,0, 239))
         ("pikachu", base_pkmn("Pikachu",25,"Mouse", "Electric","", "Static","Lightningrod","",
                               0.4,6.0, 0.5,0.5, 35,55,30,50,40,90, 0,0,0,0,0,2, 82)) //Y starter
+        ("arcanine", base_pkmn("Arcanine",59,"Legendary", "Fire","", "Intimidate","Flash Fire","Justified",
+                              1.9,155.0, 0.75,0.25, 90,110,80,100,80,95, 0,2,0,0,0,0, 213))
+        ("alakazam", base_pkmn("Alakazam",65,"Psi", "Psychic","", "Synchronize","Inner Focus","Magic Guard",
+                              1.5,48.0, 0.75,0.25, 55,50,45,135,85,120, 0,0,0,3,0,0, 186))
+        ("machamp", base_pkmn("Machamp",68,"Superpower", "Fighting","", "Guts","No Guard","Steadfast",
+                              1.6,130.0, 0.75,0.25, 90,130,80,65,85,55, 0,3,0,0,0,0, 193))
+        ("exeggutor", base_pkmn("Exeggutor",103,"Coconut", "Grass","Psychic", "Chlorophyll","Harvest","",
+                              2.0,120.0, 0.5,0.5, 95,95,85,125,65,55, 0,0,0,2,0,0, 212))
+        ("gyarados", base_pkmn("Gyarados",130,"Atrocious", "Water","Flying", "Intimidate","Moxie","",
+                              6.5,235.0, 0.5,0.5, 95,125,79,60,100,81, 0,2,0,0,0,0, 214))
+        ("lapras", base_pkmn("Lapras",131,"Transport", "Water","Ice", "Water Absorb","Shell Armor","Hydration",
+                              2.5,220.0, 0.5,0.5, 130,85,80,85,95,60, 2,0,0,0,0,0, 219))
+        ("aerodactyl", base_pkmn("Aerodactyl",142,"Fossil", "Rock","Flying", "Rock Head","Pressure","Unnerve",
+                              1.9,59.0, 0.875,0.125, 80,105,65,60,75,130, 0,0,0,0,0,2, 202))
+        ("snorlax", base_pkmn("Snorlax",143,"Sleeping", "Normal","", "Immunity","Thick Fat","Gluttony",
+                              2.1,460.0, 0.875,0.125, 160,110,65,65,110,30, 2,0,0,0,0,0, 154))
         ("chikorita", base_pkmn("Chikorita",152,"Leaf", "Grass","", "Overgrow","Leaf Guard","",
                                 0.9,6.4, 0.875,0.125, 45,49,65,49,65,45, 0,0,0,0,1,0, 64)) //GSC,HGSS starter
         ("cyndaquil", base_pkmn("Cyndaquil",155,"Fire Mouse", "Fire","", "Blaze","Flash Fire","",
