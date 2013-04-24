4055a771e5eaaf7060d2fdff79792b7c14c9bb4b
Nicholas Corgan <n.corgan@gmail.com>
2013-04-24 08:25:43 -0700

    CMake work
    * SQLITE3_FOUND is now cached
    * Changed project name from PKMNSIM to PKMNsim

--- cmake/Modules/FindSqlite3.cmake
+++ cmake/Modules/FindSqlite3.cmake
@@ -1,7 +1,7 @@
 # Find SQLite3
 # ~~~~~~~~~~~~
 # Copyright (c) 2007, Martin Dobias <wonder.sk at gmail.com>
-# Copyright (c) 2012, Nicholas Corgan <n.corgan at gmail.com>
+# Copyright (c) 2012,2013 Nicholas Corgan <n.corgan at gmail.com>
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
@@ -23,55 +23,57 @@
 
 # try to use framework on mac
 # want clean framework path, not unix compatibility path
-IF (APPLE)
-  IF (CMAKE_FIND_FRAMEWORK MATCHES "FIRST"
-      OR CMAKE_FRAMEWORK_PATH MATCHES "ONLY"
-      OR NOT CMAKE_FIND_FRAMEWORK)
-    SET (CMAKE_FIND_FRAMEWORK_save ${CMAKE_FIND_FRAMEWORK} CACHE STRING "" FORCE)
-    SET (CMAKE_FIND_FRAMEWORK "ONLY" CACHE STRING "" FORCE)
-    #FIND_PATH(SQLITE3_INCLUDE_DIR SQLite3/sqlite3.h)
-    FIND_LIBRARY(SQLITE3_LIBRARY SQLite3)
-    IF (SQLITE3_LIBRARY)
-      # FIND_PATH doesn't add "Headers" for a framework
-      SET (SQLITE3_INCLUDE_DIR ${SQLITE3_LIBRARY}/Headers CACHE PATH "Path to a file.")
-    ENDIF (SQLITE3_LIBRARY)
-    SET (CMAKE_FIND_FRAMEWORK ${CMAKE_FIND_FRAMEWORK_save} CACHE STRING "" FORCE)
-  ENDIF ()
-ENDIF (APPLE)
+IF(NOT SQLITE3_FOUND)
+    IF (APPLE)
+      IF (CMAKE_FIND_FRAMEWORK MATCHES "FIRST"
+          OR CMAKE_FRAMEWORK_PATH MATCHES "ONLY"
+          OR NOT CMAKE_FIND_FRAMEWORK)
+        SET (CMAKE_FIND_FRAMEWORK_save ${CMAKE_FIND_FRAMEWORK} CACHE STRING "" FORCE)
+        SET (CMAKE_FIND_FRAMEWORK "ONLY" CACHE STRING "" FORCE)
+        #FIND_PATH(SQLITE3_INCLUDE_DIR SQLite3/sqlite3.h)
+        FIND_LIBRARY(SQLITE3_LIBRARY SQLite3)
+        IF (SQLITE3_LIBRARY)
+          # FIND_PATH doesn't add "Headers" for a framework
+          SET (SQLITE3_INCLUDE_DIR ${SQLITE3_LIBRARY}/Headers CACHE PATH "Path to a file.")
+        ENDIF (SQLITE3_LIBRARY)
+        SET (CMAKE_FIND_FRAMEWORK ${CMAKE_FIND_FRAMEWORK_save} CACHE STRING "" FORCE)
+      ENDIF ()
+    ENDIF (APPLE)
 
-FIND_PATH(SQLITE3_INCLUDE_DIR sqlite3.h
-  "$ENV{LIB_DIR}/include"
-  "$ENV{LIB_DIR}/include/sqlite"
-  #mingw
-  c:/msys/local/include
-  NO_DEFAULT_PATH
-  )
-FIND_PATH(SQLITE3_INCLUDE_DIR sqlite3.h)
+    FIND_PATH(SQLITE3_INCLUDE_DIR sqlite3.h
+      "$ENV{LIB_DIR}/include"
+      "$ENV{LIB_DIR}/include/sqlite"
+      #mingw
+      c:/msys/local/include
+      NO_DEFAULT_PATH
+      )
+    FIND_PATH(SQLITE3_INCLUDE_DIR sqlite3.h)
 
-FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3 sqlite3_i PATHS
-  "$ENV{LIB_DIR}/lib"
-  #mingw
-  c:/msys/local/lib
-  NO_DEFAULT_PATH
-  )
-FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3)
+    FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3 sqlite3_i PATHS
+      "$ENV{LIB_DIR}/lib"
+      #mingw
+      c:/msys/local/lib
+      NO_DEFAULT_PATH
+      )
+    FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3)
 
-FIND_PROGRAM(SQLITE3_EXECUTABLE NAMES sqlite3)
+    FIND_PROGRAM(SQLITE3_EXECUTABLE NAMES sqlite3)
 
-IF (SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARY AND SQLITE3_EXECUTABLE)
-   SET(SQLITE3_FOUND TRUE)
-ENDIF (SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARY AND SQLITE3_EXECUTABLE)
+    IF (SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARY AND SQLITE3_EXECUTABLE)
+       SET(SQLITE3_FOUND TRUE CACHE BOOL "Found SQLite3")
+    ENDIF (SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARY AND SQLITE3_EXECUTABLE)
 
-IF (SQLITE3_FOUND)
+    IF (SQLITE3_FOUND)
 
-   IF (NOT SQLITE3_FIND_QUIETLY)
-      MESSAGE(STATUS "Found SQLite3:")
-      MESSAGE(STATUS " * Library: ${SQLITE3_LIBRARY}")
-      MESSAGE(STATUS " * Executable: ${SQLITE3_EXECUTABLE}")
-   ENDIF (NOT SQLITE3_FIND_QUIETLY)
+       IF (NOT SQLITE3_FIND_QUIETLY)
+          MESSAGE(STATUS "Found SQLite3:")
+          MESSAGE(STATUS " * Library: ${SQLITE3_LIBRARY}")
+          MESSAGE(STATUS " * Executable: ${SQLITE3_EXECUTABLE}")
+       ENDIF (NOT SQLITE3_FIND_QUIETLY)
 
-ELSE (SQLITE3_FOUND)
+    ELSE (SQLITE3_FOUND)
 
-  MESSAGE(FATAL_ERROR "Could not find SQLite3")
+      MESSAGE(FATAL_ERROR "Could not find SQLite3")
 
-ENDIF (SQLITE3_FOUND)
+    ENDIF (SQLITE3_FOUND)
+ENDIF(NOT SQLITE3_FOUND)
