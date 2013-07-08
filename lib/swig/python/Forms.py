41c05c9eb592b4a2c1bcc478a40dfe1a7d10cb96
Nicholas Corgan <n.corgan@gmail.com>
2013-07-07 19:04:16 -0700

    Added MIT license to manual Python pseudo-enums

--- lib/swig/python/Forms.py
+++ lib/swig/python/Forms.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+#
+# Copyright (c) 2013 Nicholas Corgan (n.corgan@gmail.com)
+#
+# Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+# or copy at http://opensource.org/licenses/MIT)
+#
 
 class Pichu:
     NORMAL = 0
