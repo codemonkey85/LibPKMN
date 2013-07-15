70db97887d9327115d5d8c7ed610d8ba863cccc6
Nicholas Corgan <n.corgan@gmail.com>
2013-07-15 01:54:11 -0700

    trainer_gen4impl: same functionality as trainer_gen3impl

--- lib/trainer_gen4impl.cpp
+++ lib/trainer_gen4impl.cpp
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 
 #include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <pkmnsim/base_move.hpp>
 #include <pkmnsim/base_pkmn.hpp>
@@ -178,6 +179,14 @@ namespace pkmnsim
 
         if(!save->parseRawSave()) cout << "Couldn't load file." << endl;
 
+        PokeLib::Trainer* pokelib_trainer = save->getTrainer();
+        money = 0; //Placeholder until money is implemented in PokeLib
+        PokeLib::widetext trainer_name_wide = pokelib_trainer->getName();
+        char trainer_name_buffer[7];
+        memset(trainer_name_buffer,0,7);
+        wcstombs(trainer_name_buffer, trainer_name_wide.c_str(), 7);
+        trainer_name = trainer_name_buffer;
+
         party.clear();
         PokeLib::Party* pokelib_party = save->getParty();
         for(unsigned int i = 0; i < (unsigned int)(pokelib_party->count()); i++)
@@ -306,7 +315,6 @@ namespace pkmnsim
         string identifier, move1, move2, move3, move4;
         int level; 
 
-        cout << "Inside convert_to_spec_pkmn" << endl;
         identifier = PokeLib::species[int(pokelib_pkmn.pkm->pkm.species)];
         level = int(pokelib_pkmn.getLevel());
 
@@ -316,6 +324,28 @@ namespace pkmnsim
         move4 = database::to_database_format(PokeLib::movelist[int(pokelib_pkmn.pkm->pkm.move[3])]);
 
         spec_pkmn::sptr s_pkmn = spec_pkmn::make(identifier, 4, level, move1, move2, move3, move4, true);
+
+        PokeLib::widetext nickname_wide = pokelib_pkmn.getNickname();
+        char nickname_buffer[10];
+        memset(nickname_buffer,0,10);
+        wcstombs(nickname_buffer, nickname_wide.c_str(), 10);
+        s_pkmn->nickname = nickname_buffer;
+
+        s_pkmn->nature = pkmn_nature::make(PokeLib::nature[int(pokelib_pkmn.getNatureValue())]);
+
+        s_pkmn->HP = pokelib_pkmn.pkm->pkm.battle_max_hp;
+        s_pkmn->ATK = pokelib_pkmn.pkm->pkm.battle_atk;
+        s_pkmn->DEF = pokelib_pkmn.pkm->pkm.battle_def;
+        s_pkmn->SATK = pokelib_pkmn.pkm->pkm.battle_satk;
+        s_pkmn->SDEF = pokelib_pkmn.pkm->pkm.battle_sdef;
+        s_pkmn->SPD = pokelib_pkmn.pkm->pkm.battle_spd;
+
+        s_pkmn->ivHP = pokelib_pkmn.pkm->pkm.iv_hp;
+        s_pkmn->ivATK = pokelib_pkmn.pkm->pkm.iv_atk;
+        s_pkmn->ivDEF = pokelib_pkmn.pkm->pkm.iv_def;
+        s_pkmn->ivSATK = pokelib_pkmn.pkm->pkm.iv_satk;
+        s_pkmn->ivSDEF = pokelib_pkmn.pkm->pkm.iv_sdef;
+        s_pkmn->ivSPD = pokelib_pkmn.pkm->pkm.iv_spd;
 
         return s_pkmn;
     }
