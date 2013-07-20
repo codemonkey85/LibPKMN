9dccd783a845ec648ab1b331a2511095d7d8d077
Nicholas Corgan <n.corgan@gmail.com>
2013-07-20 09:36:52 -0700

    PKMNsim can now be used as part of a larger project
    * CMAKE_SOURCE_DIR and CMAKE_BINARY_DIR replaced to avoid assumptions
    * Headers and applications are not installed

--- cmake/Modules/PKMNsimPackage.cmake
+++ cmake/Modules/PKMNsimPackage.cmake
@@ -5,48 +5,50 @@
 # or copy at http://opensource.org/licenses/MIT)
 #
 
-#Set CPack Generator
-IF(CPACK_GENERATOR)
-    #already set
-ELSEIF(APPLE)
-    SET(CPACK_GENERATOR PackageMaker)
-ELSEIF(WIN32)
-    SET(CPACK_GENERATOR NSIS)
-ELSEIF(LINUX AND EXISTS "/etc/debian_version")
-    SET(CPACK_GENERATOR DEB)
-ELSEIF(LINUX AND EXISTS "/etc/redhat-release")
-    SET(CPACK_GENERATOR RPM)
-ELSE()
-    SET(CPACK_GENERATOR TGZ)
-ENDIF()
+IF(NOT USED_AS_SUBMODULE) #Let parent project decide these settings
+    #Set CPack Generator
+    IF(CPACK_GENERATOR)
+        #already set
+    ELSEIF(APPLE)
+        SET(CPACK_GENERATOR PackageMaker)
+    ELSEIF(WIN32)
+        SET(CPACK_GENERATOR NSIS)
+    ELSEIF(LINUX AND EXISTS "/etc/debian_version")
+        SET(CPACK_GENERATOR DEB)
+    ELSEIF(LINUX AND EXISTS "/etc/redhat-release")
+        SET(CPACK_GENERATOR RPM)
+    ELSE()
+        SET(CPACK_GENERATOR TGZ)
+    ENDIF()
 
-########################################################################
-# Setup CPack General
-########################################################################
-SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "PKMNsim - Pokémon Simulator")
-SET(CPACK_PACKAGE_VENDOR              "Nicholas Corgan")
-SET(CPACK_PACKAGE_CONTACT             "Nicholas Corgan <n.corgan@gmail.com>")
-SET(CPACK_PACKAGE_VERSION "0.0.1")
-SET(CPACK_RESOURCE_FILE_WELCOME ${CMAKE_SOURCE_DIR}/README.txt)
-SET(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_SOURCE_DIR}/LICENSE.txt)
+    ########################################################################
+    # Setup CPack General
+    ########################################################################
+    SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "PKMNsim - Pokémon Simulator")
+    SET(CPACK_PACKAGE_VENDOR              "Nicholas Corgan")
+    SET(CPACK_PACKAGE_CONTACT             "Nicholas Corgan <n.corgan@gmail.com>")
+    SET(CPACK_PACKAGE_VERSION "0.0.1")
+    SET(CPACK_RESOURCE_FILE_WELCOME ${CMAKE_SOURCE_DIR}/README.txt)
+    SET(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_SOURCE_DIR}/LICENSE.txt)
 
-########################################################################
-# Setup Windows-specific settings
-########################################################################
-IF(WIN7_PKG)
-    #Install MSVC runtime
-    INCLUDE(InstallRequiredSystemLibraries)
-ENDIF(WIN7_PKG)
+    ########################################################################
+    # Setup Windows-specific settings
+    ########################################################################
+    IF(WIN7_PKG)
+        #Install MSVC runtime
+        INCLUDE(InstallRequiredSystemLibraries)
+    ENDIF(WIN7_PKG)
 
-IF(${CPACK_GENERATOR} STREQUAL NSIS)
-    ENABLE_LANGUAGE(C)
+    IF(${CPACK_GENERATOR} STREQUAL NSIS)
+        ENABLE_LANGUAGE(C)
 
-    include(CheckTypeSize)
-    check_type_size("void*[8]" BIT_WIDTH BUILTIN_TYPES_ONLY)
-    SET(CPACK_PACKAGE_FILE_NAME "PKMNsim_Win${BIT_WIDTH}")
+        include(CheckTypeSize)
+        check_type_size("void*[8]" BIT_WIDTH BUILTIN_TYPES_ONLY)
+        SET(CPACK_PACKAGE_FILE_NAME "PKMNsim_Win${BIT_WIDTH}")
 
-    SET(CPACK_PACKAGE_INSTALL_DIRECTORY "PKMNsim")
-ENDIF()
+        SET(CPACK_PACKAGE_INSTALL_DIRECTORY "PKMNsim")
+    ENDIF()
+ENDIF(NOT USED_AS_SUBMODULE)
 
 SET(CPACK_NSIS_MODIFY_PATH ON)
 
