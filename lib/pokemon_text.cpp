755b81e5439f422f53bb46749ea52e3a1afdb1f9
Nicholas Corgan <n.corgan@gmail.com>
2013-10-12 14:19:07 -0700

    Added pokemon_text class, which uses Qt4 internally to deal with Unicode mess when dealing with text conversions

---
+++ lib/pokemon_text.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2013 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include <ctime>
+
+#include <QString>
+
+#include <pkmnsim/types/pokemon_text.hpp>
+
+namespace pkmnsim
+{
+    pokemon_text::pokemon_text(const char* input) {set(input);}
+    pokemon_text::pokemon_text(const wchar_t* input) {set(input);}
+    pokemon_text::pokemon_text(std::string input) {set(input);}
+    pokemon_text::pokemon_text(std::wstring input) {set(input);}
+    
+    const char* pokemon_text::const_char() const {return stdstring.c_str();}
+    const wchar_t* pokemon_text::const_wchar_t() const {return stdwstring.c_str();}
+    std::string pokemon_text::std_string() const {return stdstring;}
+    std::wstring pokemon_text::std_wstring() const {return stdwstring;}
+    
+    void pokemon_text::set(const char* input)
+    {
+        QString intermediary = QString(input);
+        
+        stdstring = intermediary.toStdString();
+        
+        #ifdef _MSC_VER
+            stdwstring = std::wstring((const wchar_t*)intermediary.utf16());
+        #else
+            stdwstring = intermediary.toStdWString();
+        #endif
+    }
+    
+    void pokemon_text::set(const wchar_t* input)
+    {
+        QString intermediary;
+    
+        #ifdef _MSC_VER
+            intermediary = QString::fromUtf16((const ushort*)input);
+            stdwstring = std::wstring((const wchar_t*)intermediary.utf16());
+        #else
+            intermediary = QString::fromWCharArray(input);
+            stdwstring = intermediary.toStdWString();
+        #endif
+        
+        stdstring = intermediary.toStdString();
+    }
+    
+    void pokemon_text::set(std::string input)
+    {
+        QString intermediary = QString::fromStdString(input);
+        
+        stdstring = intermediary.toStdString();
+        
+        #ifdef _MSC_VER
+            stdwstring = std::wstring((const wchar_t*)intermediary.utf16());
+        #else
+            stdwstring = intermediary.toStdWString();
+        #endif
+    }
+    
+    void pokemon_text::set(std::wstring input)
+    {
+        QString intermediary;
+        
+        #ifdef _MSC_VER
+            intermediary = QString::fromUtf16((const ushort*)input.c_str());
+            stdwstring = std::wstring((const wchar_t*)intermediary.utf16());
+        #else
+            intermediary = QString::fromWCharArray(input);
+            stdwstring = intermediary.toStdWString();
+        #endif
+        
+        stdstring = intermediary.toStdString();
+    }
+}
