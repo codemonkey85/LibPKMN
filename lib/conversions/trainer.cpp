d81656444b7fb3ec974105cf45533989e4901448
Nicholas Corgan <n.corgan@gmail.com>
2013-11-14 17:25:47 -0800

    conversions: PKMDS uses Pokemon in party to determine save's game and trainer's gender

--- lib/conversions/trainer.cpp
+++ lib/conversions/trainer.cpp
@@ -184,6 +184,7 @@ namespace pkmnsim
                     and (pokelib_pokemon.pkm->pkm.ot_sid == pokelib_secret_id))
                 {
                     game_id = hometown_to_pkmnsim_game(pokelib_pokemon.pkm->pkm.hometown);
+                    found = true;
                     break;
                 }
             }
@@ -209,8 +210,8 @@ namespace pkmnsim
 
             trainer::sptr pkmnsim_trainer = trainer::make(game_id, trainer_name, gender);
 
-            pkmnsim_trainer->set_public_id(pokelib_save->BlockA[tOffset[offsetTID][save_type]]);
-            pkmnsim_trainer->set_secret_id(pokelib_save->BlockA[tOffset[offsetSID][save_type]]);
+            pkmnsim_trainer->set_public_id(pokelib_public_id);
+            pkmnsim_trainer->set_secret_id(pokelib_secret_id);
             pkmnsim_trainer->set_money(*(reinterpret_cast<uint32_t*>(&(pokelib_save->BlockA[tOffset[offsetMoney][save_type]]))));
 
             bag::sptr pkmnsim_bag = pkmnsim_trainer->get_bag();
@@ -251,19 +252,48 @@ namespace pkmnsim
 
         trainer::sptr import_trainer_from_pkmds_g5(pkmds_g5_sptr pkmds_save)
         {
-            pokemon_text pkmds_name = ::getsavtrainername(pkmds_save->cur);
-            trainer::sptr pkmnsim_trainer = trainer::make(Games::BLACK2, pkmds_name, Genders::MALE);
-            //TODO: distinguish Gen 5 games
-            //Gender not reverse-engineered
+            /*
+             * PKMDS has no way of distinguishing between the different Gen 5
+             * games, and trainer gender hasn't been reverse engineered. To
+             * find each, we will parse the party to determine the relevant
+             * information from Pokemon with matching ID's. If none are found,
+             * use defaults.
+             */
+            party_obj* pkmds_party = &(pkmds_save->cur.party);
+            uint16_t pkmds_public_id = pkmds_save->cur.tid;
+            uint16_t pkmds_secret_id = pkmds_save->cur.sid;
+            unsigned int game_id, gender;
+            bool found = false;
 
-            pkmnsim_trainer->set_public_id(pkmds_save->cur.tid);
-            pkmnsim_trainer->set_secret_id(pkmds_save->cur.sid);
+            for(size_t i = 0; i < pkmds_party->size; i++)
+            {
+                pokemon_obj* pkmds_pokemon = &(pkmds_party->pokemon[i].pkm_data);
+
+                if((pkmds_pokemon->tid == pkmds_public_id) and (pkmds_pokemon->sid == pkmds_secret_id))
+                {
+                    game_id = hometown_to_pkmnsim_game(int(pkmds_pokemon->hometown));
+                    gender = (get_gen4_5_otgender((reinterpret_cast<uint8_t*>(&(pkmds_pokemon->ball)+1)))) ?
+                             Genders::FEMALE : Genders::MALE;
+                    found = true;
+                    break;
+                }
+            }
+            if(not found)
+            {
+                game_id = Games::BLACK2;
+                gender = Genders::MALE;
+            }
+
+            pokemon_text pkmds_name = ::getsavtrainername(pkmds_save->cur);
+            trainer::sptr pkmnsim_trainer = trainer::make(game_id, pkmds_name, gender);
+
+            pkmnsim_trainer->set_public_id(pkmds_public_id);
+            pkmnsim_trainer->set_secret_id(pkmds_secret_id);
             pkmnsim_trainer->set_money(0);
             
             bag::sptr pkmnsim_bag = pkmnsim_trainer->get_bag();
             pkmnsim_bag = import_items_from_pkmds_g5(&(pkmds_save->cur.bag), Games::BLACK2);
 
-            party_obj* pkmds_party = &(pkmds_save->cur.party);
             for(size_t i = 0; i < pkmds_party->size; i++)
             {
                 pkmnsim_trainer->set_pokemon(i+1, pkmds_pokemon_to_team_pokemon(&(pkmds_party->pokemon[i])));
