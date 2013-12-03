b18c7222770c5a327f76847a73297d377c5a6678
Nicholas Corgan <n.corgan@gmail.com>
2013-12-03 15:30:21 -0800

    io: added export_to_pksql (doesn't include attributes)

--- lib/io.cpp
+++ lib/io.cpp
@@ -9,6 +9,9 @@
 #include <fstream>
 #include <string.h>
 
+#include <boost/format.hpp>
+
+#include <pkmnsim/enums.hpp>
 #include <pkmnsim/io.hpp>
 
 #include <pkmds/pkmds_g5.h>
@@ -16,6 +19,8 @@
 
 #include "library_bridge.hpp"
 #include "conversions/pokemon.hpp"
+
+#include "SQLiteCpp/src/SQLiteC++.h"
 
 namespace pkmnsim
 {
@@ -83,6 +88,97 @@ namespace pkmnsim
             
             pkmnsim_pctopartyx(p_pkm, pkmn_obj);
             return conversions::pkmds_g6_pokemon_to_team_pokemon(p_pkm);
+        }
+
+        void export_to_pksql(team_pokemon::sptr t_pkmn, std::string filename)
+        {
+            dict<unsigned int, unsigned int> stats = t_pkmn->get_stats();
+            dict<unsigned int, unsigned int> EVs = t_pkmn->get_EVs();
+            dict<unsigned int, unsigned int> IVs = t_pkmn->get_IVs();
+
+            SQLite::Database pksql_db(filename.c_str(), (SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE));
+            //TODO: attributes
+            std::string create_table = "BEGIN TRANSACTION;\n"
+                                       "CREATE TABLE pokemon (\n"
+                                       "    id INTEGER NOT NULL,\n"
+                                       "    species_id INTEGER NOT NULL,\n"
+                                       "    game_id INTEGER NOT NULL,\n"
+                                       "    nickname VARCHAR(20) NOT NULL,\n"
+                                       "    otname VARCHAR(20) NOT NULL,\n"
+                                       "    held_item INTEGER NOT NULL,\n"
+                                       "    ball INTEGER NOT NULL,\n"
+                                       "    level INTEGER NOT NULL,\n"
+                                       "    met_level INTEGER NOT NULL,\n"
+                                       "    ability INTEGER NOT NULL,\n"
+                                       "    nature INTEGER NOT NULL,\n"
+                                       "    personality INTEGER NOT NULL,\n"
+                                       "    trainer_id INTEGER NOT NULL,\n"
+                                       "    hp INTEGER NOT NULL,\n"
+                                       "    attack INTEGER NOT NULL,\n"
+                                       "    defense INTEGER NOT NULL,\n"
+                                       "    speed INTEGER NOT NULL,\n"
+                                       "    special INTEGER NOT NULL,\n"
+                                       "    spatk INTEGER NOT NULL,\n"
+                                       "    spdef INTEGER NOT NULL,\n"
+                                       "    ev_hp INTEGER NOT NULL,\n"
+                                       "    ev_attack INTEGER NOT NULL,\n"
+                                       "    ev_defense INTEGER NOT NULL,\n"
+                                       "    ev_speed INTEGER NOT NULL,\n"
+                                       "    ev_special INTEGER NOT NULL,\n"
+                                       "    ev_spatk INTEGER NOT NULL,\n"
+                                       "    ev_spdef INTEGER NOT NULL,\n"
+                                       "    iv_hp INTEGER NOT NULL,\n"
+                                       "    iv_attack INTEGER NOT NULL,\n"
+                                       "    iv_defense INTEGER NOT NULL,\n"
+                                       "    iv_speed INTEGER NOT NULL,\n"
+                                       "    iv_special INTEGER NOT NULL,\n"
+                                       "    iv_spatk INTEGER NOT NULL,\n"
+                                       "    iv_spdef INTEGER NOT NULL,\n"
+                                       "    is_female INTEGER NOT NULL,\n"
+                                       "    ot_is_female INTEGER NOT NULL\n"
+                                       ");\n"
+                                       "COMMIT;";
+            std::string pokemon_export =
+                str(boost::format("INSERT INTO \"pokemon\" VALUES(0,%d,%d,'%s','%s',%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d);")
+                        % t_pkmn->get_species_id()
+                        % t_pkmn->get_game_id()
+                        % t_pkmn->get_nickname().const_char()
+                        % t_pkmn->get_trainer_name().const_char()
+                        % t_pkmn->get_held_item()->get_item_id()
+                        % t_pkmn->get_ball()
+                        % t_pkmn->get_level()
+                        % t_pkmn->get_met_level()
+                        % t_pkmn->get_ability()
+                        % t_pkmn->get_nature()
+                        % t_pkmn->get_personality()
+                        % t_pkmn->get_trainer_id()
+                        % stats[Stats::HP]
+                        % stats[Stats::ATTACK]
+                        % stats[Stats::DEFENSE]
+                        % stats[Stats::SPEED]
+                        % stats.get(Stats::SPECIAL, 0)
+                        % stats.get(Stats::SPECIAL_ATTACK, 0)
+                        % stats.get(Stats::SPECIAL_DEFENSE, 0)
+                        % EVs[Stats::HP]
+                        % EVs[Stats::ATTACK]
+                        % EVs[Stats::DEFENSE]
+                        % EVs[Stats::SPEED]
+                        % EVs.get(Stats::SPECIAL, 0)
+                        % EVs.get(Stats::SPECIAL_ATTACK, 0)
+                        % EVs.get(Stats::SPECIAL_DEFENSE, 0)
+                        % IVs[Stats::HP]
+                        % IVs[Stats::ATTACK]
+                        % IVs[Stats::DEFENSE]
+                        % IVs[Stats::SPEED]
+                        % IVs.get(Stats::SPECIAL, 0)
+                        % IVs.get(Stats::SPECIAL_ATTACK, 0)
+                        % IVs.get(Stats::SPECIAL_DEFENSE, 0)
+                        % ((t_pkmn->get_gender() == Genders::FEMALE) ? 1 : 0)
+                        % ((t_pkmn->get_trainer_gender() == Genders::FEMALE) ? 1 : 0)
+                    );
+
+            pksql_db.exec(create_table.c_str());
+            pksql_db.exec(pokemon_export.c_str());
         }
     }
 }
