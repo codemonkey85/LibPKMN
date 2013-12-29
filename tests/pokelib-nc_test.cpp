0a585009d7d0ffa3b7a0cb4b47b8e6dce199a966
Nicholas Corgan <n.corgan@gmail.com>
2013-12-28 22:11:40 -0800

    tests: finished Pokemon stats portion of pokelib-nc_test

--- tests/pokelib-nc_test.cpp
+++ tests/pokelib-nc_test.cpp
@@ -30,5 +30,21 @@ BOOST_AUTO_TEST_CASE(pkmnsim_to_pokelib_DPPt)
 
     PokeLib::Pokemon pokelib_pkmn = pkmnsim::conversions::team_pokemon_to_pokelib_pokemon(pkmnsim_pkmn);
 
-    BOOST_CHECK(pkmnsim_pkmn->get_level() == 50);
+    pokelib_pkmn.updateBattleStats();
+    pkmnsim::dict<unsigned int, unsigned int> stats = pkmnsim_pkmn->get_stats();
+
+    std::cout << std::endl << "PokeLib vs. LibPKMNsim stats" << std::endl;
+    std::cout << " * " << int(pokelib_pkmn.pkm->pkm.battle_max_hp) << " - " << stats[pkmnsim::Stats::HP] << std::endl;
+    std::cout << " * " << int(pokelib_pkmn.pkm->pkm.battle_atk) << " - " << stats[pkmnsim::Stats::ATTACK] << std::endl;
+    std::cout << " * " << int(pokelib_pkmn.pkm->pkm.battle_def) << " - " << stats[pkmnsim::Stats::DEFENSE] << std::endl;
+    std::cout << " * " << int(pokelib_pkmn.pkm->pkm.battle_satk) << " - " << stats[pkmnsim::Stats::SPECIAL_ATTACK] << std::endl;
+    std::cout << " * " << int(pokelib_pkmn.pkm->pkm.battle_sdef) << " - " << stats[pkmnsim::Stats::SPECIAL_DEFENSE] << std::endl;
+    std::cout << " * " << int(pokelib_pkmn.pkm->pkm.battle_spd) << " - " << stats[pkmnsim::Stats::SPEED] << std::endl;
+
+    BOOST_CHECK_EQUAL(pokelib_pkmn.pkm->pkm.battle_max_hp, stats[pkmnsim::Stats::HP]);
+    BOOST_CHECK_EQUAL(pokelib_pkmn.pkm->pkm.battle_atk, stats[pkmnsim::Stats::ATTACK]);
+    BOOST_CHECK_EQUAL(pokelib_pkmn.pkm->pkm.battle_def, stats[pkmnsim::Stats::DEFENSE]);
+    BOOST_CHECK_EQUAL(pokelib_pkmn.pkm->pkm.battle_satk, stats[pkmnsim::Stats::SPECIAL_ATTACK]);
+    BOOST_CHECK_EQUAL(pokelib_pkmn.pkm->pkm.battle_sdef, stats[pkmnsim::Stats::SPECIAL_DEFENSE]);
+    BOOST_CHECK_EQUAL(pokelib_pkmn.pkm->pkm.battle_spd, stats[pkmnsim::Stats::SPEED]);
 }
