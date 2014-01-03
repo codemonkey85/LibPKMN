ad74a755bf510af7b8bcff9dd4257799f0959011
Nicholas Corgan <n.corgan@gmail.com>
2014-01-02 20:42:29 -0800

    Started Pokemon-sim rename to LibPKMN
    * PKMNsim library changed to PKMN
    * CMake scripts changed accordingly

--- cmake/Modules/LibPKMNComponent.cmake
+++ cmake/Modules/LibPKMNComponent.cmake
@@ -5,8 +5,8 @@
 # or copy at http://opensource.org/licenses/MIT)
 #
 
-set(_pkmnsim_enabled_components "" CACHE INTERNAL "" FORCE)
-set(_pkmnsim_disabled_components "" CACHE INTERNAL "" FORCE)
+set(_libpkmn_enabled_components "" CACHE INTERNAL "" FORCE)
+set(_libpkmn_disabled_components "" CACHE INTERNAL "" FORCE)
 
 ########################################################################
 # Register a component into the system
@@ -16,14 +16,14 @@ set(_pkmnsim_disabled_components "" CACHE INTERNAL "" FORCE)
 #  - deps a list of dependencies
 #  - dis the default disable setting
 ########################################################################
-MACRO(PKMNSIM_REGISTER_COMPONENT name var enb deps dis)
-    IF(NOT PKMNSIM_USED_AS_SUBMODULE)
+MACRO(LIBPKMN_REGISTER_COMPONENT name var enb deps dis)
+    IF(NOT LIBPKMN_USED_AS_SUBMODULE)
         MESSAGE(STATUS "")
         MESSAGE(STATUS "Determining support for ${name}.")
         FOREACH(dep ${deps})
             MESSAGE(STATUS " - Dependency ${dep} = ${${dep}}")
         ENDFOREACH(dep)
-    ENDIF(NOT PKMNSIM_USED_AS_SUBMODULE)
+    ENDIF(NOT LIBPKMN_USED_AS_SUBMODULE)
 
     #setup the dependent option for this component
     INCLUDE(CMakeDependentOption)
@@ -31,43 +31,43 @@ MACRO(PKMNSIM_REGISTER_COMPONENT name var enb deps dis)
 
     #append the component into one of the lists
     IF(${var})
-        IF(NOT PKMNSIM_USED_AS_SUBMODULE)
+        IF(NOT LIBPKMN_USED_AS_SUBMODULE)
             MESSAGE(STATUS " - Enabling ${name}.")
-        ENDIF(NOT PKMNSIM_USED_AS_SUBMODULE)
-        LIST(APPEND _pkmnsim_enabled_components ${name})
+        ENDIF(NOT LIBPKMN_USED_AS_SUBMODULE)
+        LIST(APPEND _libpkmn_enabled_components ${name})
     ELSE(${var})
-        IF(NOT PKMNSIM_USED_AS_SUBMODULE)
+        IF(NOT LIBPKMN_USED_AS_SUBMODULE)
             MESSAGE(STATUS " - Disabling ${name}.")
-        ENDIF(NOT PKMNSIM_USED_AS_SUBMODULE)
-        LIST(APPEND _pkmnsim_disabled_components ${name})
+        ENDIF(NOT LIBPKMN_USED_AS_SUBMODULE)
+        LIST(APPEND _libpkmn_disabled_components ${name})
     ENDIF(${var})
-    IF(NOT PKMNSIM_USED_AS_SUBMODULE)
+    IF(NOT LIBPKMN_USED_AS_SUBMODULE)
         MESSAGE(STATUS " - Override with -D${var}=ON/OFF")
-    ENDIF(NOT PKMNSIM_USED_AS_SUBMODULE)
+    ENDIF(NOT LIBPKMN_USED_AS_SUBMODULE)
 
     #make components lists into global variables
-    SET(_pkmnsim_enabled_components ${_pkmnsim_enabled_components} CACHE INTERNAL "" FORCE)
-    SET(_pkmnsim_disabled_components ${_pkmnsim_disabled_components} CACHE INTERNAL "" FORCE)
-ENDMACRO(PKMNSIM_REGISTER_COMPONENT)
+    SET(_libpkmn_enabled_components ${_libpkmn_enabled_components} CACHE INTERNAL "" FORCE)
+    SET(_libpkmn_disabled_components ${_libpkmn_disabled_components} CACHE INTERNAL "" FORCE)
+ENDMACRO(LIBPKMN_REGISTER_COMPONENT)
 
 #Print the summary of enabled/disabled components
-FUNCTION(PKMNSIM_PRINT_COMPONENT_SUMMARY)
+FUNCTION(LIBPKMN_PRINT_COMPONENT_SUMMARY)
     MESSAGE(STATUS "")
     MESSAGE(STATUS "######################################################")
-    MESSAGE(STATUS "# Pokemon-sim - Enabled Components                    ")
+    MESSAGE(STATUS "# LibPKMN - Enabled Components                        ")
     MESSAGE(STATUS "######################################################")
-    FOREACH(comp ${_pkmnsim_enabled_components})
+    FOREACH(comp ${_libpkmn_enabled_components})
         MESSAGE(STATUS " - ${comp}")
     ENDFOREACH(comp)
 
     MESSAGE(STATUS "")
     MESSAGE(STATUS "######################################################")
-    MESSAGE(STATUS "# Pokemon-sim - Disabled Components                   ")
+    MESSAGE(STATUS "# LibPKMN - Disabled Components                       ")
     MESSAGE(STATUS "######################################################")
-    FOREACH(comp ${_pkmnsim_disabled_components})
+    FOREACH(comp ${_libpkmn_disabled_components})
         MESSAGE(STATUS " - ${comp}")
     ENDFOREACH(comp)
 
     MESSAGE(STATUS "")
-ENDFUNCTION(PKMNSIM_PRINT_COMPONENT_SUMMARY)
+ENDFUNCTION(LIBPKMN_PRINT_COMPONENT_SUMMARY)
 
