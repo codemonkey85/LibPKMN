a4cd3b7b6d5fe3e8f168f9b5c7721e113f6f1024
Nicholas Corgan <n.corgan@gmail.com>
2014-02-18 12:00:26 -0800

    Finished C# pokemon_text <-> string cast and corresponding unit test

--- lib/swig/cs/pokemon_text_partial.cs
+++ lib/swig/cs/pokemon_text_partial.cs
@@ -1,12 +1,31 @@
-namespace LibPKMN {
+/*
+ * Copyright (c) 2014 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
 
 using System;
 
-public partial class pokemon_text : IDisposable {
-  public override string ToString() {
-    string ret = tostring();
-    return ret;
-  }
-}
+namespace LibPKMN
+{
+    public partial class pokemon_text : IDisposable
+    {
+        public override string ToString()
+        {
+            string ret = tostring();
+            return ret;
+        }
 
+        public static implicit operator string(pokemon_text input)
+        {
+            return input.tostring();
+        }
+
+        public static implicit operator pokemon_text(string input)
+        {
+            pokemon_text temp = new pokemon_text(input);
+            return temp;
+        }
+    }
 }
--- tests/CSharpPokemonTextTest.cs
+++ tests/CSharpPokemonTextTest.cs
@@ -12,8 +12,41 @@ public class PokemonTextTest
 {
     public static int Main(string[] args)
     {
-        LibPKMN.pokemon_text test_text = new LibPKMN.pokemon_text("Test");
-        System.Console.WriteLine(test_text); //If this compiles and prints, test works
+        //Testing printing on command line
+        LibPKMN.pokemon_text test_text1 = new LibPKMN.pokemon_text("Test1");
+        System.Console.WriteLine(test_text1);
+
+        //Testing string -> LibPKMN.pokemon_text
+        string test_string1 = "Test2";
+        LibPKMN.pokemon_text test_text2 = test_string1; //implicit
+        if(test_text2 != "Test2")
+        {
+            throw new System.Exception("C# string -> LibPKMN.pokemon_text implicit conversion failed!");
+        }
+        LibPKMN.pokemon_text test_text3 = (LibPKMN.pokemon_text)test_string1; //explicit
+        if(test_text3 != "Test2")
+        {
+            throw new System.Exception("C# string -> LibPKMN.pokemon_text explicit conversion failed!");
+        }
+
+        //Testing LibPKMN.pokemon_text -> string
+        LibPKMN.pokemon_text test_text4 = new LibPKMN.pokemon_text("Test3");
+        string test_string2 = test_text4.ToString();
+        if(test_string2 != "Test3")
+        {
+            throw new System.Exception("LibPKMN.pokemon_text.ToString() failed!");
+        }
+        string test_string3 = test_text4;
+        if(test_string3 != "Test3")
+        {
+            throw new System.Exception("LibPKMN.pokemon_text -> C# string implicit conversion failed!");
+        }
+        string test_string4 = (string)test_text4;
+        if(test_string4 != "Test3")
+        {
+            throw new System.Exception("LibPKMN.pokemon_text -> C# string explicit conversion failed!");
+        }
+
         return 0;
     }    
 }
