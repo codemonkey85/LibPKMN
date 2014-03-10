4045746f6ca6a42f6a66d67495d89ff3286f83db
Nicholas Corgan <n.corgan@gmail.com>
2014-03-10 09:42:35 -0700

    item: machine fixes
    * Database queries include version groups so not all values come from Gen VI
    * Fixed off-by-one error with HM database ID's

--- lib/item_machineimpl.cpp
+++ lib/item_machineimpl.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Nicholas Corgan (n.corgan@gmail.com)
+ * Copyright (c) 2013-2014 Nicholas Corgan (n.corgan@gmail.com)
  *
  * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
  * or copy at http://opensource.org/licenses/MIT)
@@ -24,10 +24,10 @@ namespace pkmn
     item_machineimpl::item_machineimpl(unsigned int id, unsigned int game): item_impl(id, game)
     {
         if(_item_id >= Items::TM01 and _item_id <= Items::TM92) _machine_id = _item_id - 304;
-        else _machine_id = 100 + (_item_id - Items::HM01);
+        else _machine_id = 101 + (_item_id - Items::HM01);
         
-        string query_string = "SELECT move_id FROM machines WHERE machine_number="
-                            + to_string(_machine_id);
+        string query_string(str(boost::format("SELECT move_id FROM machines WHERE machine_number=%d AND version_group_id=%d")
+                                % _machine_id % database::get_version_group(_game_id)));
         _move_id = int(_db.execAndGet(query_string.c_str()));
         
         _item_name = str(boost::format("%s (%s)") % database::get_item_name(_item_id)
