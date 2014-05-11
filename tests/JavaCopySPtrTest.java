5d48485768fdb8fe8c6653276b6dd7b93e21ae20
Nicholas Corgan <n.corgan@gmail.com>
2014-05-10 19:03:50 -0700

    Can call getDatabasePath from Java

--- tests/JavaCopySPtrTest.java
+++ tests/JavaCopySPtrTest.java
@@ -11,6 +11,7 @@ public class JavaCopySPtrTest
 {
     public static void main(String[] args)
     {
-        System.out.println("foo");
+        String path = Core.getDatabasePath();
+        System.out.println(path);
     }
 }
