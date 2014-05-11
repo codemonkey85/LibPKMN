759b37553511c9f5ab2b21c9de0483a0e3a2649e
Nicholas Corgan <n.corgan@gmail.com>
2014-05-10 19:00:29 -0700

    Added Java unit test structure, can import LibPKMN, doesn't call anything from it yet

---
+++ tests/JavaCopySPtrTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2014 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+import org.nc.LibPKMN.*;
+
+public class JavaCopySPtrTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("foo");
+    }
+}
